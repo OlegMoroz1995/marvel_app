ede76c7c01ce70414e09ea99bb96838ed299c795
Oleh <moroz.oleg1995@gmail.com>
2022-12-19 17:11:35 +0200

    version_1

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import RandomChar from "../randomCharacter/randomCharacter";
+import CharList from "../characterList/characterList";
+import CharInfo from "../characterInfo/characterInfo";
+import ErrorBoundary from "../errorBoundary/errorBoundary";
+
+import decoration from '../../resourses/img/vision.png';
+
+const MainPage = () => {
+
+    const [selectedChar, setChar] = useState(null);
+
+    const onCharSelected = (id) => {
+        setChar(id);
+    }
+
+    return (
+        <>
+            <ErrorBoundary>
+                <RandomChar/>
+            </ErrorBoundary>
+            <div className="char__content">
+                <ErrorBoundary>
+                    <CharList onCharSelected={onCharSelected}/>
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <CharInfo charId={selectedChar}/>
+                </ErrorBoundary>
+            </div>
+            <img className="bg-decoration" src={decoration} alt="vision"/>
+        </>
+    )
+}
+
+export default MainPage;
